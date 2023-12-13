38ba8b4f04c8d8ae12397369ee2bcc7619eccbdf
Aura <darsodios@gmail.com>
2023-12-13 14:07:22 +0100

    Pause First implementation

---
+++ Assets/Scripts/Enum/GameStates.cs
@@ -0,0 +1,6 @@
+public enum GameStates
+{
+    Gameplay, Pause
+}
+
+//Small enum to check the current game state, will expand in the future if necessary
---
+++ Assets/Scripts/SystemScripts/PauseGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Script to handle pausing and unpausing the game
+public class PauseGame : MonoBehaviour
+{
+    public void Pause()
+    {
+
+    }
+
+    public void UnPause()
+    {
+
+    }
+}
