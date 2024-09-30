24388197884d3a78d58ad24737f2cafa370a8ee7
Marcos Somoza <somozadev@gmail.com>
2024-09-30 09:26:40 +0100

    Finished lvl 0 -- check text for modifications
    Missing in lvl 0 :
    - improved text
    - some 3d models

---
+++ Assets/Scripts/DialogueScripts/DialogueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using Ink.Runtime;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DialogueTrigger : MonoBehaviour
+{
+    
+    [SerializeField] private TextAsset dialogue;
+    private bool _interacted; 
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.tag.Equals("Player") || _interacted) return;
+        other.GetComponentInParent<Rigidbody>().velocity = Vector3.zero;
+        GameManager.instance.currentController.playerConversationComponent.SetCurrentDialogue(new Story(dialogue.text));
+        GameManager.instance.currentController.ChangeState(PlayerState.Conversation);
+        _interacted = true;
+    }
+    
+    
+}
