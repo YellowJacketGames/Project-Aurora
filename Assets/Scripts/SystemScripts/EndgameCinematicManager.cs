23dcd0750a05ffc220fda753e50da1f9c75f204a
somozadev <somozadev@gmail.com>
2024-12-30 10:20:43 +0100

    Added endgame cutscene + several bug fixes

---
+++ Assets/Scripts/SystemScripts/EndgameCinematicManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+
+public class EndgameCinematicManager : MonoBehaviour
+{
+    [SerializeField] VideoPlayer player;
+    float videoLength;
+
+    public bool skip;
+    void Start()
+    {
+        GameManager.instance.currentTransitionManager.SetFadeOut();
+        videoLength = (float)player.clip.length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            skip = true;
+        }
+        videoLength -= Time.deltaTime;
+
+        if (videoLength <= Time.deltaTime || skip)
+        {
+            GameManager.instance.SetLevelToLoad("MainMenu_Rework");
+            GameManager.instance.ResetData();
+            GameManager.instance.currentTransitionManager.NextLevel();
+            player.Stop();
+        }
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene("LoadingScreen");
+
+    }
+
+    public void SkipCutscene()
+    {
+        skip = true;
+    }
+}
